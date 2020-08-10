17ce98fea58ff37b1ef8d4cfe547fce61284ef39
Kirill Shashichev <qtrixnet@yandex.kz>
2020-08-10 16:28:32 +0600

    Исправление ошибок после 1го ревью

--- sripts/sript.js
+++ sripts/sript.js
@@ -3,35 +3,25 @@ const popupForm = document.querySelector('.popup__container');               // 
 const popupOpenButton = document.querySelector('.profile__edit-button');     // Находим кнопку редактирования профиля
 const popupCloseButton = popup.querySelector('.popup__close-button');        // Находим кнопку закрытия попапа
 const popupSaveButton = popup.querySelector('.popup__save-button');          // Находим кнопку сохранения
-const nameInput = document.querySelector('.popup__name');                    // Находим инпут с именем
-const jobInput = document.querySelector('.popup__job');                      // Находим инпут с профессией
+const nameInput = popupForm.querySelector('.popup__input[name="name"]')      // Находим инпут с именем
+const jobInput = popupForm.querySelector('.popup__input[name="job"]')        // Находим инпут с профессией
+const profileName = document.querySelector('.profile__name');                // Находим элемент для вставки значений
+const profileJob = document.querySelector('.profile__job');                  // Находим элемент для вставки значений
 
 const popupToggle = function () {                                            // Функция добавления / удаления класса видимости у попапа
+  nameInput.value = profileName.textContent                                  // Помещаем Имя в инпут формы
+  jobInput.value = profileJob.textContent                                    // Помещаем Профессию в инпут формы
   popup.classList.toggle('popup_opened'); 
 }
 
-// Помещаем текстовое содержимое элементов в константы
-const namePlaceholder = profile__name.textContent;                           // Имя
-const jobPlaceholder = profile__job.textContent;                             // Профессия    
-
-// Подставляем значения элементов профиля в инпуты
-nameInput.setAttribute('value', namePlaceholder);                            // Имя
-jobInput.setAttribute('value', jobPlaceholder);                              // Профессия
-
 function formSubmitHandler (evt) {
-  // Отменяем стандартное поведение формы
-  evt.preventDefault(); 
-
-  // Выбираем элементы для вставки значений
-  const profileName = document.querySelector('.profile__name');
-  const profileJob = document.querySelector('.profile__job');
+  evt.preventDefault();                                                      // Отменяем стандартное поведение формы
 
   // Вставляем значения инпутов в выбранные элементы
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
 
-  // Закрываем попап
-  popupToggle();
+  popupToggle();                                                             // Закрываем попап
 }
 
 // Слушатели
