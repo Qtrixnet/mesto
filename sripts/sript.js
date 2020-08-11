f80520b4e19b44d991970c7ac46f8bc8c0d5921a
Kirill Shashichev <qtrixnet@yandex.kz>
2020-08-11 11:01:39 +0600

    Исправление ошибок ревью 2

--- sripts/sript.js
+++ sripts/sript.js
@@ -3,15 +3,21 @@ const popupForm = document.querySelector('.popup__container');               // 
 const popupOpenButton = document.querySelector('.profile__edit-button');     // Находим кнопку редактирования профиля
 const popupCloseButton = popup.querySelector('.popup__close-button');        // Находим кнопку закрытия попапа
 const popupSaveButton = popup.querySelector('.popup__save-button');          // Находим кнопку сохранения
-const nameInput = popupForm.querySelector('.popup__input[name="name"]')      // Находим инпут с именем
-const jobInput = popupForm.querySelector('.popup__input[name="job"]')        // Находим инпут с профессией
+const nameInput = popupForm.querySelector('.popup__input[name="name"]');      // Находим инпут с именем
+const jobInput = popupForm.querySelector('.popup__input[name="job"]');        // Находим инпут с профессией
 const profileName = document.querySelector('.profile__name');                // Находим элемент для вставки значений
 const profileJob = document.querySelector('.profile__job');                  // Находим элемент для вставки значений
 
-const popupToggle = function () {                                            // Функция добавления / удаления класса видимости у попапа
-  nameInput.value = profileName.textContent                                  // Помещаем Имя в инпут формы
-  jobInput.value = profileJob.textContent                                    // Помещаем Профессию в инпут формы
-  popup.classList.toggle('popup_opened'); 
+const popupToggle = function () {
+  if (!popup.classList.contains('popup_opened')) {                           // Функция добавления / удаления класса видимости у попапа
+
+  nameInput.value = profileName.textContent;                                  // Помещаем Имя в инпут формы
+  jobInput.value = profileJob.textContent;                                    // Помещаем Профессию в инпут формы
+  popup.classList.toggle('popup_opened');
+
+  } else {
+    popup.classList.toggle('popup_opened');
+  }
 }
 
 function formSubmitHandler (evt) {
